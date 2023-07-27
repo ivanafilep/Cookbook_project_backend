b83454ae8cf7562bbbbc5e9eab8d10f8d4bdff4c
RajkovicAleksa <125392649+RajkovicAleksa@users.noreply.github.com>
2023-07-27 16:13:21 +0200

    ChefController

---
+++ src/main/java/com/praksa/team4/repositories/ChefRepository.java
@@ -0,0 +1,10 @@
+package com.praksa.team4.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.praksa.team4.entities.Chef;
+
+
+public interface ChefRepository extends CrudRepository<Chef, Integer> {
+
+}
