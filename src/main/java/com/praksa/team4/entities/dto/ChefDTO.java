fb6d941331d8cc0de8c8312c81026d3ca0bd0c83
Isidora Havlovic <isidorahavlovic@gmail.com>
2023-08-06 15:48:27 +0200

    Fixed ChefController/DTO/Service
    Added isActive and fixed security

--- src/main/java/com/praksa/team4/entities/dto/ChefDTO.java
+++ src/main/java/com/praksa/team4/entities/dto/ChefDTO.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.praksa.team4.entities.Chef;
 import com.praksa.team4.entities.Recipe;
 
 public class ChefDTO {
@@ -32,19 +33,13 @@ public class ChefDTO {
 		super();
 	}
 
-	public ChefDTO(
-			@NotNull(message = "Username must be specified") @Size(min = 2, max = 30, message = "User name must be between {min} and {max} characters long.") String username,
-			@Pattern(regexp = "^(?=.[0-9])(?=.[a-z])(?=.*[A-Z]).{8,100}$", message = "Password must be at least 8 characters long and contain a lowercase, an upercase letter and a number") @NotNull(message = "Password must be specified") @Size(min = 8, max = 100, message = "Password must be between {min} and {max} characters long.") String password,
-			@NotNull(message = "Name must be included.") @Size(min = 2, max = 30, message = "Name must be between {min} and {max} characters long.") String name,
-			@NotNull(message = "Lastname must be included.") @Size(min = 2, max = 30, message = "Lastname must be between {min} and {max} characters long.") String lastname,
-			@NotNull(message = "Email must be included.") @Pattern(regexp = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", message = "Email is not valid.") String email,
-			List<Recipe> recipes) {
+	public ChefDTO(Chef c) {
 		super();
-		this.username = username;
-		this.name = name;
-		this.lastname = lastname;
-		this.email = email;
-		this.recipes = recipes;
+		this.username = c.getUsername();
+		this.name = c.getName();
+		this.lastname = c.getLastname();
+		this.email = c.getEmail();
+		this.recipes = c.getRecipes();
 	}
 
 	public String getUsername() {
