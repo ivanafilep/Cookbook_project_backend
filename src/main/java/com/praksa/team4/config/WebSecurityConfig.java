c1c0036e273bebbdc1f5c9bf82a30ca42656cedb
ivanafilep <ivana.filep.brains22@gmail.com>
2023-08-07 15:06:21 +0200

    Encryption - added automatic encryption

--- src/main/java/com/praksa/team4/config/WebSecurityConfig.java
+++ src/main/java/com/praksa/team4/config/WebSecurityConfig.java
@@ -8,8 +8,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
@@ -29,6 +32,11 @@ public class WebSecurityConfig {
 	public SecretKey secretKey() {
 		return this.secretKey;
 	}
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }	
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
